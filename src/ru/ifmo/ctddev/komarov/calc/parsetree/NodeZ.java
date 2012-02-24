ce6e845b4e1a106cea1d17ea661a8eb29f3e208d
Andrey Komarov <taukus@gmail.com>
2012-02-25 02:30:32 +0400

    +забытое NodeZ

---
+++ src/ru/ifmo/ctddev/komarov/calc/parsetree/NodeZ.java
@@ -0,0 +1,20 @@
+package ru.ifmo.ctddev.komarov.calc.parsetree;
+
+import ru.ifmo.ctddev.komarov.calc.exceptions.OverflowException;
+
+import static ru.ifmo.ctddev.komarov.calc.Checker.checkIfOverflow;
+
+/**
+* Created by IntelliJ IDEA.
+* User: komarov
+* Date: 2/17/12
+* Time: 2:39 PM
+* To change this template use File | Settings | File Templates.
+*/
+public class NodeZ implements ParseTreeNode {
+    @Override
+    public double evaluate(double x, double y, double z) throws OverflowException {
+        checkIfOverflow(z);
+        return z;
+    }
+}
